390ffbf12d3ffdf9d68355004eb66dae5fdcc821
kimby <kimby@brandi.co.kr>
2019-09-14 23:41:21 +0900

    OkHttp로 api 호출, 응답 테스트

--- app/src/main/java/com/example/weatherm/MainActivity.java
+++ app/src/main/java/com/example/weatherm/MainActivity.java
@@ -3,6 +3,17 @@ package com.example.weatherm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.weatherm.api.HttpConnection;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
         // hihi
         // push
+
+        HttpConnection.getInstance().requestWebServer("Seoul", new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d("HttpConnection", "onFailure");
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d("HttpConnection", "onResponse");
+                Log.d("HttpConnection", "response : " + response.body().string());
+            }
+        });
     }
 }
