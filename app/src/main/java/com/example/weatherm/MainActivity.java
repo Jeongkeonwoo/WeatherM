a3e5c7c6e24f63d867eb85bcdce113d7d272ed28
kimby <kimby@brandi.co.kr>
2019-09-25 21:30:29 +0900

    OkHttp로 주석 작성

--- app/src/main/java/com/example/weatherm/MainActivity.java
+++ app/src/main/java/com/example/weatherm/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.example.weatherm.api.HttpConnection;
+import com.example.weatherm.api_okhttp.HttpConnection;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -22,20 +22,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // hihi
-        // push
+        // 통신을 할 땐, manifest에 인터넷 허용 권한을 넣어줘야함
+        // OkHttp로 통신 시작
+        HttpConnection.getInstance().requestWebServer(new Callback() {
 
-        HttpConnection.getInstance().requestWebServer("Seoul", new Callback() {
+            // 응답 실패했을 때 실행되는 콜백 메소드
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
                 Log.d("HttpConnection", "onFailure");
             }
 
+            // 응답 성곡했을 때 실행되는 콜백 메소드
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 Log.d("HttpConnection", "onResponse");
+
+                // 응답받는 response 데이터의 body 를 로그출력
                 Log.d("HttpConnection", "response : " + response.body().string());
             }
         });
+        // OkHttp로 통신 끝
     }
 }
