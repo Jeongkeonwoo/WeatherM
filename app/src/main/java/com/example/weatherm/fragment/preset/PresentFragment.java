c5b950393de43b5e3b45e6951541f7f405d3b2f7
kimby <kimby@brandi.co.kr>
2019-10-05 22:58:08 +0900

    merge 겹친 파일 수정

--- app/src/main/java/com/example/weatherm/fragment/preset/PresentFragment.java
+++ app/src/main/java/com/example/weatherm/fragment/preset/PresentFragment.java
@@ -1,10 +1,12 @@
 package com.example.weatherm.fragment.preset;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.weatherm.Data;
+import com.example.weatherm.MainActivity;
 import com.example.weatherm.R;
 import com.example.weatherm.WeatherData;
 import com.example.weatherm.WeatherManager;
@@ -22,26 +25,41 @@ import com.example.weatherm.WeatherUtil;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+
 public class PresentFragment extends Fragment {
 
+    private MainActivity activity;
+
     private RecyclerView recyclerView;
+    private TextView presentWeatherTemperature;
+    private TextView presentWeatherCity;
+    private ImageView presentWeatherIcon;
+
     private PresentAdapter presentAdapter;
-    private  final int WEATHER = 24;
-    private ProgressDialog pd;//view 에서 다른 view 로 넘어갈때 로딩
+    private final int WEATHER = 24;
     private WeatherManager weatherManager;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        activity = (MainActivity) context;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.fragment_present, null);
-        //TextView textView = view.findViewById(R.id.present_weather_temperature);
+
+        presentWeatherTemperature = view.findViewById(R.id.present_weather_temperature);
+        presentWeatherCity = view.findViewById(R.id.present_weather_city);
+        presentWeatherIcon = view.findViewById(R.id.present_weather_icon);
         recyclerView = view.findViewById(R.id.present_recyclerview);
 
         ArrayList<Data> data = new ArrayList<>();
 
-        for (int i = 0; i < WEATHER; i++ ){
-//            data.add(new Data(R.mipmap.ic_launcher, i+1+""));
+        for (int i = 0; i < WEATHER; i++) {
+//            data.add(new Data(R.mipmap.ic_launcher, i + 1 + ""));
         }
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
@@ -54,40 +72,33 @@ public class PresentFragment extends Fragment {
         recyclerView.setAdapter(presentAdapter);
 
 
-//        showProgress("날씨 요청 중입니다.");
+        // api 요청
+        activity.showProgress("날씨 요청 중입니다.");
 
-//        weatherManager = new WeatherManager(this, new WeatherManager.OnChangeWeather() {
-//            @Override
-//            public void change(WeatherData weatherData) {
-//                hideProgress();
-//
-//                String temp = WeatherUtil.getCelsius(weatherData.getMain().getTemp());
-//                //   tvTemp.setText(temp);
-//            }
-//        });
+        weatherManager = new WeatherManager(activity, new WeatherManager.OnChangeWeather() {
+            @Override
+            public void change(WeatherData weatherData) {
+                // api 응답 완료시 실행
+                activity.hideProgress();
+                load(weatherData);
+            }
+        });
 
-//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         return view;
     }
 
-    public void showProgress(String message) {
-        if (pd != null) {
-            pd.dismiss();
-            pd = null;
-        }
+    private void load(WeatherData weatherData) {
 
-        //pd = new ProgressDialog(this);
-        pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        pd.setMessage(message);
-        pd.setCancelable(false);
-        pd.setCanceledOnTouchOutside(false);
-        pd.show();
-    }
+        // 현재 도시 이름
+        presentWeatherCity.setText(weatherData.getName());
 
-    public void hideProgress() {
-        if (pd != null) {
-            pd.dismiss();
-            pd = null;
-        }
+        // 현재 날씨 이미지
+        int weatherId = weatherData.getWeather().get(0).getId();
+        int imageResource = WeatherUtil.getWeatherIcon(weatherId);
+        presentWeatherIcon.setImageResource(imageResource);
+
+        // 현재 온도
+        String temp = WeatherUtil.getCelsius(weatherData.getMain().getTemp());
+        presentWeatherTemperature.setText(temp + "˚");
     }
 }
