09268c34ed350453ac2cd7dd140807861ba7a3c5
minwoo <redalsdn7@naver.com>
2019-10-18 20:39:02 +0900

    에러 발생

--- app/src/main/java/com/example/weatherm/fragment/daytime/DaytimeAdapter.java
+++ app/src/main/java/com/example/weatherm/fragment/daytime/DaytimeAdapter.java
@@ -1,5 +1,6 @@
 package com.example.weatherm.fragment.daytime;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,18 +10,26 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.weatherm.WeatherUtil;
 import com.example.weatherm.data.Data;
 import com.example.weatherm.R;
+import com.example.weatherm.data.ForecastData;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DaytimeAdapter extends RecyclerView.Adapter<DaytimeAdapter.DaytimeHolder> {
 
-    private ArrayList<Data> dataList;
-
-    public void setDataList(ArrayList<Data> dataList) {
-        this.dataList = dataList;
+//    private List<Data> dataList;
+    private List<ForecastData.ListBean> forecastList;
+    //Adapter 에 전잘해 사용할 List 형의 데이터
+/////////////////////////////////////////////////////////////
+    public DaytimeAdapter(List<ForecastData.ListBean> forecastList){
+        this.forecastList = forecastList;
     }
+//    public void setDataList(ArrayList<Data> dataList) {
+//        this.dataList = dataList;
+//    }
 
     @NonNull
     @Override
@@ -37,18 +46,35 @@ public class DaytimeAdapter extends RecyclerView.Adapter<DaytimeAdapter.DaytimeH
     //onCreateViewHolder() 에서 holder 를 return 받은 holder 에 데이터를 셋팅해준다.
     @Override
     public void onBindViewHolder(@NonNull DaytimeHolder holder, int position) {
-        Data data = dataList.get(position);
+//        Data data = dataList.get(position);
 
-        holder.daytime_weather_weekly.setText(data.getWeekly());
-        holder.daytime_weather_date.setText(data.getDate());
-        holder.daytime_weather_icon.setImageResource(data.getImg());
-        holder.daytime_weather_hightemperature.setText(data.getHight());
-        holder.daytime_weather_lowtemperature.setText(data.getLow());
+        ForecastData.ListBean forecastBean = forecastList.get(position);
+
+        Log.d("DaytimeAdapter","getHour"+ WeatherUtil.getHour(forecastBean.getDt_txt()));
+
+        holder.daytime_weather_date.setText(WeatherUtil.getHour(forecastBean.getDt_txt()));
+
+        int weatherId = forecastBean.getWeather().get(0).getId();
+        int imageResource = WeatherUtil.getWeatherIcon(weatherId);
+        holder.daytime_weather_icon.setImageResource(imageResource);
+
+        String temp = WeatherUtil.getCelsius(forecastBean.getMain().getTemp());
+        holder.daytime_weather_hightemperature.setText(temp+"˚");
+//        holder.daytime_weather_weekly.setText(data.getWeekly());
+//        //요일
+//        holder.daytime_weather_date.setText(data.getDate());
+//        //날짜
+//        holder.daytime_weather_icon.setImageResource(data.getImg());
+//        //날씨이미지
+//        holder.daytime_weather_hightemperature.setText(data.getHight());
+//        //최고온도
+//        holder.daytime_weather_lowtemperature.setText(data.getLow());
+//        //최저온도
     }
 
     @Override
     public int getItemCount() {
-        return dataList.size();
+        return forecastList.size();
     }
 
     class DaytimeHolder extends RecyclerView.ViewHolder {
@@ -63,10 +89,15 @@ public class DaytimeAdapter extends RecyclerView.Adapter<DaytimeAdapter.DaytimeH
             super(itemView);
 
             daytime_weather_weekly = itemView.findViewById(R.id.daytime_weather_weekly);
+            //요일
             daytime_weather_date = itemView.findViewById(R.id.daytime_weather_date);
+            //날짜
             daytime_weather_icon = itemView.findViewById(R.id.daytime_weather_icon);
+            //이미지 날씨
             daytime_weather_hightemperature = itemView.findViewById(R.id.daytime_weather_hightemperature);
+            //최고온도
             daytime_weather_lowtemperature = itemView.findViewById(R.id.daytime_weather_lowtemperature);
+            //최저온도
         }
     }
 }
--- app/src/main/java/com/example/weatherm/fragment/daytime/DaytimeFragment.java
+++ app/src/main/java/com/example/weatherm/fragment/daytime/DaytimeFragment.java
@@ -1,5 +1,6 @@
 package com.example.weatherm.fragment.daytime;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,39 +12,73 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.weatherm.MainActivity;
+import com.example.weatherm.WeatherManager;
 import com.example.weatherm.data.Data;
 import com.example.weatherm.R;
+import com.example.weatherm.data.ForecastData;
+import com.example.weatherm.data.WeatherData;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DaytimeFragment extends Fragment {
 
+    private  MainActivity activity;
+
     private RecyclerView recyclerView;
     private DaytimeAdapter daytimeAdapter;
-    private final int day = 8;
+    private WeatherManager weatherManager;
+    // 3시간별로 출력할 날씨 개수
+    private final int WEATHER_COUNT = 10;
+
+    //일주일 별로 출력할 날씨 개수
+    private final int day = 7;
+
+    //context 를 매개변수로 받아 context 를 MainActivity 타입으로 강제 형변환.?
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        activity = (MainActivity) context;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+        //view 를 객체화 시킴
         View view = inflater.inflate(R.layout.fragment_daytime, null);
         //TextView textView = view.findViewById(R.id.text);
         recyclerView = view.findViewById(R.id.daytime_recyclerview);
 
-        ArrayList<Data> data = new ArrayList<>();
+//        ArrayList<Data> data = new ArrayList<>();
+         weatherManager = new WeatherManager(activity, new WeatherManager.OnChangeWeather() {
+            @Override
+            public void change(WeatherData weatherData, ForecastData forecastData) {
+                //api 응답 완료시 실행.
+                activity.hideProgress();
 
-        for (int i = 0; i < day ; i++){
-            //data.add(new Data(R.mipmap.ic_launcher, ""));
+                loadWeekly(forecastData);
+            }
+        });
+        return view;
+    }
+    private void loadWeekly(ForecastData forecastData) {
+
+        List<ForecastData.ListBean> forecastList = new ArrayList<>();
+
+        for (int i = 0; i < WEATHER_COUNT; i++) {
+            forecastList.add(forecastData.getList().get(i));
         }
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(linearLayoutManager);
 
-        daytimeAdapter = new DaytimeAdapter();
+        daytimeAdapter = new DaytimeAdapter(forecastList);
 
-        daytimeAdapter.setDataList(data);
+        //daytimeAdapter.setDataList(data);
 
         recyclerView.setAdapter(daytimeAdapter);
-
-        return view;
     }
+
 }
